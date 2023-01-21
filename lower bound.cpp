ecc997003148e90a8bf5a5f48f7479479869ca95
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2023-01-22 02:58:53 +0530

    Create lower bound.cpp

---
+++ lower bound.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(int arr[],int n,int x)
+{
+    int low=0,high=n-1;
+    while(low<=high)
+    {
+        int mid = (low+high)/2;
+        if(arr[mid]==x) return mid;
+        if(arr[mid]>x) high = mid-1;
+        if(arr[mid]<x) low = mid+1;   
+    }
+    return -1;
+}
+
+int lowerbound(int arr[],int n,int x)
+{
+    int ans = -1;
+    int low =0;
+    int high = n-1;
+    while(low<=high)
+    {
+        int mid = (low+high)/2;
+        if(arr[mid]>=x)
+        {
+          ans = mid;
+          high = mid-1;
+        }
+        else
+        {
+            low = mid+1;
+        }
+    }
+    return ans;
+}
+int main(){
+int n,x;
+cout<<"Enter the size of the array: "<<endl;
+cin>>n;
+int arr[n];
+cout<<"Enter the elements of the array: "<<endl;
+for(int i=0;i<n;i++)
+{
+    cin>>arr[i];
+}
+cout<<"Enter the required element needed to be searched: "<<endl;
+cin>>x;
+cout<<lowerbound(arr,n,x);
+return 0;
+}
